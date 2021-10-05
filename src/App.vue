531783e88bb4ae1cb27c0a51d8b72413b81576eb
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-10-05 16:57:25 +0900

    TodoListの作成

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <TodoList />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TodoList,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="todolist">
+    <h1>Todoアプリ</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <span>{{ todo.item }}</span>
+      </li>
+    </ul>
+    <button v-on:click="showTodo">＋</button>
+    <div v-if="toggle">aaa</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data: function() {
+    return {
+      newItem: "",
+      toggle: true,
+      todos: [
+        {
+          index: 1,
+          item: "aaa",
+        },
+        {
+          index: 2,
+          item: "bbb",
+        },
+      ],
+    };
+  },
+  methods: {
+    showTodo: function() {
+      this.toggle == true ? (this.toggle = false) : (this.toggle = true);
+    },
+  },
+};
+</script>
